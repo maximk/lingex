9320cc35125418c1a1691526d8f9b7c073478232
mk <mk@pod.site>
2013-04-11 02:35:20 +0300

    wip

--- lib/mix.ex
+++ lib/mix.ex
@@ -1,8 +1,36 @@
-defmodule Mix.Tasks.Lingex do
+
+defmodule Mix.Tasks.Lingex.Build do
+	def run(_args) do
+		Mix.shell.info "build.run() called"
+
+		project = Mix.project
+		opts = project[:lingex_opts]
+
+		compile_path = project[:compile_path]
+		files = Path.wildcard Path.join compile_path, "*"
+
+		deps_path = project[:deps_path]
+		files = Enum.reduce project[:deps], files, fn({name, _, _}, acc) ->
+			dir = Path.join [deps_path,name,"ebin/*"]
+			acc ++ Path.wildcard dir
+		end
+
+		Enum.each files, fn(f) ->
+			IO.puts f
+		end
+
+		IO.inspect opts
+	end
 end
 
-defmodule Mix.Tasks.Lingex.build do
+defmodule Mix.Tasks.Lingex.Image do
 	def run(_Args) do
-		Io.puts "build.run()"
+		Mix.shell.info "image.run() called"
+	end
+end
+
+defmodule Mix.Tasks.Lingex.Build_image do
+	def run(_Args) do
+		Mix.shell.info "build_image.run() called"
 	end
 end
